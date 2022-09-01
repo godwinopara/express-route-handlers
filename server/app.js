2c418a9bd90ea07a3317ca8d5d18d74504a96e8c
godwin opara <godwinopara62@yahoo.com>
2022-09-01 18:07:50 +0100

    added routes for getting artist,adding artist, latest artists and latst artist album

--- server/app.js
+++ server/app.js
@@ -18,13 +18,36 @@ const {
   getSongBySongId,
   addSongByAlbumId,
   editSongBySongId,
-  deleteSongBySongId
-} = require('./data');
+  deleteSongBySongId,
+} = require("./data");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Your code here
+app.use(express.json());
+
+app.get("/artists", (req, res) => {
+  const artists = getAllArtists();
+  res.json({ body: artists });
+});
+
+app.get("/artists/latest", (req, res) => {
+  const latestArtists = getLatestArtist();
+  res.json({ body: latestArtists });
+});
+
+app.get("/artists/latest/albums", (req, res) => {
+  const latestArtistAlbums = getAlbumsForLatestArtist();
+  res.json({ body: latestArtistAlbums });
+});
+
+app.post("/artists", (req, res) => {
+  const newArtist = addArtist({ name: "Led Zeppelin" });
+  res.status(201);
+  res.json(newArtist);
+});
 
 const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+app.listen(port, () => console.log("Server is listening on port", port));
+
